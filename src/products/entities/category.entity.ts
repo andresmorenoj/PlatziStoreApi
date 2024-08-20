f2f77ad4ecada0f1150ac8150df66a6d86d3d17e
Andres Moreno <andresmorenojf@gmail.com>
2024-08-20 17:07:04 -0500

    created many to many connection between products and categories

--- src/products/entities/category.entity.ts
+++ src/products/entities/category.entity.ts
@@ -1,4 +1,13 @@
-import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  Entity,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToMany,
+} from 'typeorm';
+
+import { Product } from './product.entity';
 @Entity()
 export class Category {
   @PrimaryGeneratedColumn()
@@ -6,4 +15,13 @@ export class Category {
 
   @Column({ type: 'varchar', length: 235, unique: true })
   name: string;
+
+  @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+
+  @ManyToMany(() => Product, (category) => category.categories)
+  products: Product[];
 }
